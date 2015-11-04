20dc246acc3c4c0f7d6d83c1dc0cc7001d1e4d26
jhperales <jhperales@NN123-HA-17.HS.sd203.org>
2015-11-04 15:01:00 -0600

    i dont know
    added something

---
+++ ranodmness.py
@@ -0,0 +1,12 @@
+#randomness
+
+from Tkinter import *
+
+class Application(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.grid()
+        self.create()
+
+    def create(self):
+        
